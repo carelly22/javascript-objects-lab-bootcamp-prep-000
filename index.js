5ecf60b002d2172f66b69c2c017859a0d1389df3
Carell Hamil <ayannahamil@gmail.com>
2018-11-17 23:58:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const recipes = {};
 //define a function which updates the key-value pairs of the recipes object
 function updateObjectWithKeyAndValue(recipes, key, value){
   return Object.assign(recipes, {key:value});
+};
+
+//define a function which destructively updates the recipes object
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+  return recipes;
 }
